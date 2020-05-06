0a7bf9edbc56016f25d9486ef97481de1015c0b5
Thomas Lin <t.lin@mail.utoronto.ca>
2020-05-05 20:03:14 -0400

    Add flags and options for generating new key and writing it to file   - Reading key not done yet, will currently use the hard-coded key   - Currently supports the 4 main algos from libp2p's crypto

--- bootstrap.go
+++ bootstrap.go
@@ -2,15 +2,103 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/multiformats/go-multiaddr"
-	"github.com/libp2p/go-libp2p-core/crypto"
+)
+
+var (
+	genKey = flag.Bool("genkey", false,
+		"Generate a new key and save to file.")
+	algo = flag.String("algo", "RSA",
+		"Cryptographic algorithm to use for generating the key.\n"+
+			"Will be ignored if 'gen-key' is false.\n"+
+			"Must be one of {RSA, Ed25519, Secp256k1, ECDSA}")
+	bits = flag.Int("bits", 2048,
+		"Key length, in bits. Will be ignored if 'algo' is not RSA.")
+	keyFile = flag.String("keyfile", "~/.privKey",
+		"Location of private key to read from (or write to, if generating).")
+
+	// Map algos to enum from libp2p-core/crypto/key
+	// See: https://github.com/libp2p/go-libp2p-core/blob/master/crypto/key.go
+	// TODO: Figure out future-proof way to maintain this?
+	keyTypes = map[string]int{
+		"rsa":       crypto.RSA,
+		"ed25519":   crypto.Ed25519,
+		"secp256k1": crypto.Secp256k1,
+		"ecdsa":     crypto.ECDSA,
+	}
 )
 
 func main() {
+	flag.Parse()
+
+	// TODO: Clean up this init section
+	//       Move to function + cleaner error msg and exit mechanism
+	if *genKey {
+		var keyType int
+		for algoName, algoID := range keyTypes {
+			if strings.EqualFold(algoName, *algo) {
+				keyType = algoID
+				break
+			}
+			keyType = -1
+		}
+
+		if keyType < 0 {
+			fmt.Println("ERROR: Unknown algorithm")
+			os.Exit(1)
+		} else if keyType == crypto.RSA && *bits < 1024 {
+			fmt.Println("ERROR: Number of bits for RSA must be at least 1024")
+			os.Exit(1)
+		}
+
+		// Generate key, then write to file
+		priv, _, err := crypto.GenerateKeyPair(keyType, *bits)
+		if err != nil {
+			panic(err)
+		}
+
+        if strings.HasPrefix(*keyFile, "~") {
+			home, err := os.UserHomeDir()
+			if err != nil {
+				panic(err)
+			}
+			*keyFile = home + (*keyFile)[1:]
+		}
+
+		_, err = os.Stat(*keyFile)
+		if !os.IsNotExist(err) {
+			fmt.Println("ERROR: A key file already exists at", *keyFile)
+			fmt.Println("       Delete it or move it before proceeding")
+			os.Exit(1)
+		}
+
+		file, err := os.Create(*keyFile)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		rawBytes, err := priv.Raw()
+		if err != nil {
+			panic(err)
+		}
+		_, err = file.WriteString(crypto.ConfigEncodeKey(rawBytes))
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		// TODO: Read key from file
+		fmt.Println("read from file...")
+	}
+
 	ctx := context.Background()
 	// Set your own keypair
 	// priv, _, err := crypto.GenerateKeyPair(
@@ -23,8 +111,7 @@ func main() {
 	//     return cfg.Apply(Peerstore(pstoremem.NewPeerstore("QmZJexJchxXt71N9bSkj6PchxyGEs9g6Qvrj7fEk32FqDs")))
 	// }
 
-
-	byteVal := []byte {211, 151, 127, 224, 159, 14, 157, 18, 23, 132, 211, 171, 4, 8, 125, 131, 235, 83, 169, 205, 79, 230, 32, 138, 150, 179, 103, 28, 152, 240, 11, 111, 101, 134, 246, 174, 231, 186, 183, 172, 59, 180, 89, 156, 126, 43, 240, 153, 190, 62, 31, 24, 209, 96, 245, 188, 19, 240, 39, 95, 93, 41, 140, 38}
+	byteVal := []byte{211, 151, 127, 224, 159, 14, 157, 18, 23, 132, 211, 171, 4, 8, 125, 131, 235, 83, 169, 205, 79, 230, 32, 138, 150, 179, 103, 28, 152, 240, 11, 111, 101, 134, 246, 174, 231, 186, 183, 172, 59, 180, 89, 156, 126, 43, 240, 153, 190, 62, 31, 24, 209, 96, 245, 188, 19, 240, 39, 95, 93, 41, 140, 38}
 	priv, err := crypto.UnmarshalEd25519PrivateKey(byteVal)
 	if err != nil {
 		panic(err)
@@ -43,5 +130,5 @@ func main() {
 		panic(err)
 	}
 
-	select{}
+	select {}
 }
