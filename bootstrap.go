a9f057a4eacb7f681b53a9721046582f30afe716
Thomas Lin <t.lin@mail.utoronto.ca>
2020-05-06 12:07:18 -0400

    Added ability for bootstrap to read from existing key file   - Modified format key was stored in file to include key type   - Added functions in util file to read from key file   - Other misc. refactoring of util file (factoring out common code, etc.)

--- bootstrap.go
+++ bootstrap.go
@@ -5,10 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	//"strings"
 
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/multiformats/go-multiaddr"
 )
@@ -29,40 +27,35 @@ var (
 func main() {
 	flag.Parse()
 
-	// TODO: Clean up this init section
-	//       Move to function + cleaner error msg and exit mechanism
 	if *genKey {
+		fmt.Println("Generating a new key...")
 		err := generateKey(*algo, *bits, *keyFile)
 		if err != nil {
 			fmt.Printf("ERROR: Unable to generate key\n%v", err)
 			os.Exit(1)
 		}
-	} else {
-		// TODO: Read key from file
-		fmt.Println("read from file...")
+
+		fmt.Println("New key is stored at:", *keyFile)
 	}
 
-	ctx := context.Background()
-	// Set your own keypair
-	// priv, _, err := crypto.GenerateKeyPair(
-	// 	crypto.Ed25519, // Select your key type. Ed25519 are nice short
-	// 	-1,             // Select key length when possible (i.e. RSA).
-	// )
-	// priv, err := rsa.GenerateKey(rand.Reader, bits)
-	// fmt.Println("Private key INFO:", priv)
-	// var DefaultPeerstore Option = func(cfg *Config) error {
-	//     return cfg.Apply(Peerstore(pstoremem.NewPeerstore("QmZJexJchxXt71N9bSkj6PchxyGEs9g6Qvrj7fEk32FqDs")))
-	// }
+	if !fileExists(*keyFile) {
+		fmt.Printf("ERROR: Key (%s) does not exist.\n", *keyFile)
+		fmt.Printf("Ensure path is correct or generate a new key with -genkey.\n")
+		os.Exit(1)
+	}
 
-	byteVal := []byte{211, 151, 127, 224, 159, 14, 157, 18, 23, 132, 211, 171, 4, 8, 125, 131, 235, 83, 169, 205, 79, 230, 32, 138, 150, 179, 103, 28, 152, 240, 11, 111, 101, 134, 246, 174, 231, 186, 183, 172, 59, 180, 89, 156, 126, 43, 240, 153, 190, 62, 31, 24, 209, 96, 245, 188, 19, 240, 39, 95, 93, 41, 140, 38}
-	priv, err := crypto.UnmarshalEd25519PrivateKey(byteVal)
+	priv, err := loadPrivKeyFromFile(*keyFile)
 	if err != nil {
-		panic(err)
+		fmt.Printf("ERROR: Unable to load key from file\n%v", err)
+		os.Exit(1)
 	}
+
 	listenAddrs, err := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/4001")
 	if err != nil {
 		panic(err)
 	}
+
+	ctx := context.Background()
 	node, err := libp2p.New(ctx, libp2p.ListenAddrs(listenAddrs), libp2p.Identity(priv))
 	if err != nil {
 		panic(err)
