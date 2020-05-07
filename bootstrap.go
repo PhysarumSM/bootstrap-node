22dd7e9af42c7b3f8a18c71e46ac9ff6954ee995
Thomas Lin <t.lin@mail.utoronto.ca>
2020-05-07 11:48:00 -0400

    Added an "ephemeral mode" (pass '-ephemeral' when running the binary) to generate a new key each run without saving it to file. This could be an easy way to easily inflate the P2P network with many nodes.

--- bootstrap.go
+++ bootstrap.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/multiformats/go-multiaddr"
 )
@@ -16,25 +17,32 @@ var (
 		"Generate a new key and save to file.")
 	algo = flag.String("algo", "RSA",
 		"Cryptographic algorithm to use for generating the key.\n"+
-			"Will be ignored if 'gen-key' is false.\n"+
+			"Will be ignored if 'genkey' is false.\n"+
 			"Must be one of {RSA, Ed25519, Secp256k1, ECDSA}")
 	bits = flag.Int("bits", 2048,
 		"Key length, in bits. Will be ignored if 'algo' is not RSA.")
 	keyFile = flag.String("keyfile", "~/.privKey",
 		"Location of private key to read from (or write to, if generating).")
+	ephemeral = flag.Bool("ephemeral", false,
+		"Generate a new key just for this run, and don't store it to file.\n"+
+			"If 'keyfile' is specified, it will be ignored.")
 )
 
 func main() {
 	flag.Parse()
 
-	if *genKey {
+	var priv crypto.PrivKey
+	var err error
+	if *genKey || *ephemeral {
 		fmt.Println("Generating a new key...")
-		priv, err := generatePrivKey(*algo, *bits)
+		priv, err = generatePrivKey(*algo, *bits)
 		if err != nil {
 			fmt.Printf("ERROR: Unable to generate key\n%v", err)
 			os.Exit(1)
 		}
+	}
 
+	if *genKey && !(*ephemeral) {
 		if err = storePrivKeyToFile(priv, *keyFile); err != nil {
 			fmt.Printf("ERROR: Unable to save key to file %s\n", *keyFile)
 			os.Exit(1)
@@ -42,16 +50,18 @@ func main() {
 		fmt.Println("New key is stored at:", *keyFile)
 	}
 
-	if !fileExists(*keyFile) {
-		fmt.Printf("ERROR: Key (%s) does not exist.\n", *keyFile)
-		fmt.Printf("Ensure path is correct or generate a new key with -genkey.\n")
-		os.Exit(1)
-	}
+	if !(*ephemeral) {
+		if !fileExists(*keyFile) {
+			fmt.Printf("ERROR: Key (%s) does not exist.\n", *keyFile)
+			fmt.Printf("Ensure path is correct or generate a new key with -genkey.\n")
+			os.Exit(1)
+		}
 
-	priv, err := loadPrivKeyFromFile(*keyFile)
-	if err != nil {
-		fmt.Printf("ERROR: Unable to load key from file\n%v", err)
-		os.Exit(1)
+		priv, err = loadPrivKeyFromFile(*keyFile)
+		if err != nil {
+			fmt.Printf("ERROR: Unable to load key from file\n%v", err)
+			os.Exit(1)
+		}
 	}
 
 	listenAddrs, err := multiaddr.NewMultiaddr("/ip4/0.0.0.0/tcp/4001")
